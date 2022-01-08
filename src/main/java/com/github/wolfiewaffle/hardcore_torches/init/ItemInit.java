32503d1f6d80a062593876c45c0766fd77cb9f3a
Wolfie Waffle <wolfie.waffle11@gmail.com>
2022-01-07 20:44:43 -0700

    Add Oil Can resources

--- src/main/java/com/github/wolfiewaffle/hardcore_torches/init/ItemInit.java
+++ src/main/java/com/github/wolfiewaffle/hardcore_torches/init/ItemInit.java
@@ -2,6 +2,7 @@ package com.github.wolfiewaffle.hardcore_torches.init;
 
 import com.github.wolfiewaffle.hardcore_torches.MainMod;
 import com.github.wolfiewaffle.hardcore_torches.item.LanternItem;
+import com.github.wolfiewaffle.hardcore_torches.item.OilCanItem;
 import com.github.wolfiewaffle.hardcore_torches.item.TorchItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -12,6 +13,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemInit {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MainMod.MOD_ID);
+
+    public static final RegistryObject<Item> OIL_CAN = ITEMS.register("oil_can", () -> new OilCanItem(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
 
     public static final RegistryObject<Item> LIT_TORCH = ITEMS.register("lit_torch", () -> new TorchItem(BlockInit.LIT_TORCH.get(), BlockInit.LIT_WALL_TORCH.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
     public static final RegistryObject<Item> UNLIT_TORCH = ITEMS.register("unlit_torch", () -> new TorchItem(BlockInit.UNLIT_TORCH.get(), BlockInit.UNLIT_WALL_TORCH.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
---
+++ src/main/java/com/github/wolfiewaffle/hardcore_torches/item/OilCanItem.java
@@ -0,0 +1,10 @@
+package com.github.wolfiewaffle.hardcore_torches.item;
+
+import net.minecraft.world.item.Item;
+
+public class OilCanItem extends Item {
+
+    public OilCanItem(Properties properties) {
+        super(properties);
+    }
+}
