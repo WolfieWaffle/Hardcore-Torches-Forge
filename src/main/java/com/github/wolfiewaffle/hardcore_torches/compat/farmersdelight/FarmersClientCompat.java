20812d10235eeaf19e3d651a52c63950706587d3
Wolfie Waffle <wolfie.waffle11@gmail.com>
2026-04-29 17:55:04 -0600

    Fix Farmer's Delight crash

--- src/main/java/com/github/wolfiewaffle/hardcore_torches/compat/farmersdelight/FarmersClientCompat.java
+++ src/main/java/com/github/wolfiewaffle/hardcore_torches/compat/farmersdelight/FarmersClientCompat.java
@@ -1,13 +1,13 @@
 package com.github.wolfiewaffle.hardcore_torches.compat.farmersdelight;
 
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
-import vectorwing.farmersdelight.client.renderer.StoveRenderer;
+import vectorwing.farmersdelight.client.renderer.DefaultStoveRenderer;
 
 public class FarmersClientCompat {
     public FarmersClientCompat() {
     }
 
     public static void loadClientCompat() {
-        BlockEntityRenderers.register(FarmersCommonCompat.STOVE_BLOCK_ENTITY.get(), StoveRenderer::new);
+        BlockEntityRenderers.register(FarmersCommonCompat.STOVE_BLOCK_ENTITY.get(), DefaultStoveRenderer::new);
     }
 }
